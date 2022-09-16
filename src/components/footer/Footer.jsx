9e3b6403b8be3614d2648541a4ac2b3aafd7d633
Demilade <demiayoola@gmail.com>
2022-09-16 18:37:24 +0100

    comit on 'main'

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,11 +6,11 @@ function Footer() {
     <div> <footer class="site-footer">
     <div className="container">
       <div className="row">
-        <div className="col-sm-12 flecx col-md-3">
+        <div className="col-sm-12 col-12 flecx col-md-3">
          <div> <h1>Dream<strong><span className='brand-color'>Cinemas</span></strong></h1>
           </div>
                   </div>
-                  <div className=" col-sm-6 col-md-3 addpad"><h6>COMPANY</h6>
+                  <div className=" col-sm-6 col-12 col-md-3 addpad"><h6>COMPANY</h6>
           <ul className="footer-links">
             <li><a href="">Cinemas</a></li>
             <li><a href="">Ticket Prices</a></li>
@@ -19,7 +19,7 @@ function Footer() {
           </div>
 
 
-        <div className="col-sm-6 col-md-3">
+        <div className="col-sm-6 col-12 col-md-3">
           <h6>TERMS</h6>
           <ul className="footer-links">
             <li><a href="">General</a></li>
@@ -29,7 +29,7 @@ function Footer() {
            </ul>
         </div>
 
-        <div className="col-sm-6 col-md-3">
+        <div className="col-sm-6 col-md-3 col-12">
           <h6>Quick Links</h6>
           <ul className="footer-links">
             <li><a href="">Contact Us</a></li>
@@ -43,12 +43,12 @@ function Footer() {
     </div>
     <div className="container">
       <div className="row">
-        <div className="col-md-8 col-sm-6 col-sm-12">
+        <div className="col-md-8 col-sm-6 col-12">
           <p className="copyright-text">Copyright &copy; 2022 All Rights Reserved by Demilade Ayoola.
           </p>
         </div>
 
-        <div className="col-md-4 col-sm-6 col-xs-12">
+        <div className="col-md-4 col-sm-6 col-12">
           <ul className="social-icons">
             <li><a className="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
             <li><a className="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
