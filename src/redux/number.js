f67a753ca9ea6e6f9c1860ef8e03e912d4f2d1c0
Demilade <demiayoola@gmail.com>
2022-10-24 18:49:24 +0100

    I made use of redux to store some variables

---
+++ src/redux/number.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const userSlice = createSlice({
+  name: "number",
+  initialState: {
+    numbers: "",
+  },
+  reducers: {
+    setNumbers: (state, action) => {
+      state.numbers = action.payload;
+    }
+  },
+});
+export const { setNumbers} = userSlice.actions;
+export default userSlice.reducer;
