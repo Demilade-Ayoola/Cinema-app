4dfe01eb3bac7670a2bfc49bae1e90f12cc0c143
Demilade <demiayoola@gmail.com>
2022-10-24 19:04:00 +0100

    Added new pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '../node_modules/font-awesome/css/font-awesome.min.css'
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux"
+import userSlice from "./redux/number"
 
+let store = configureStore({
+    reducer :{number:userSlice}
+  })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
+    <Provider store={store}>
     <App />
-  
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
